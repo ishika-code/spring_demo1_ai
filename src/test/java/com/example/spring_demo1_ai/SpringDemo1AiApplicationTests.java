4a862bf01d53b5898b57ce3305c807e67b03278b
ishika-code <71772589+ishika-code@users.noreply.github.com>
2025-06-11 12:05:11 +0530

    feat: BTS-35- AI generated code src/test/java/com/example/spring_demo1_ai/SpringDemo1AiApplicationTests.java

---
+++ src/test/java/com/example/spring_demo1_ai/SpringDemo1AiApplicationTests.java
@@ -0,0 +1,1 @@
+package com.example.spring_demo1_ai; import org.junit.Test; import org.junit.runner.RunWith; import org.springframework.boot.test.context.SpringBootTest; import org.springframework.test.context.junit4.SpringRunner; @RunWith(SpringRunner.class) @SpringBootTest public class SpringDemo1AiApplicationTests { @Test public void contextLoads() { } }
